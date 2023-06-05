983e958b7ea5fbde53557c190d2cd09a369da597
Eligio Biasi <ex_98@live.it>
2023-06-05 15:48:22 +0200

    added array of images and index

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      elements: ['pippo','pluto','paperino','paperone'],
+      imagesContainer: [
+        './img/img.1.jpg',
+        './img/img.2.jpg',
+        './img/img.3.jpg',
+        './img/img.4.jpg',
+        './img/img.5.jpeg'
+      ],
+
+      indexImage:2,
     }
+  },
+
+  methods: {
+
   }
 }).mount('#app');
